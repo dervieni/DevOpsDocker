31813e39c95b1f7da864a633f9c3d00d1c512758
ed <>
2023-05-15 13:29:57 +0200

    2. Anpassungen

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-	res.send('Hello FS2023 DevOps Course, now with adjustments :-)!!!');
+	res.send('Hello FS2023 DevOps Course, now with adjustments, volume 2 :-)!!!');
 });
 
 app.listen(PORT, HOST);
