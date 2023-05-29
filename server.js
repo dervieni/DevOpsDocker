58b9a7d38ef1a041c641f0d43633d07be69cc2c1
ed <>
2023-05-29 16:09:03 +0200

    Node App Azure Set-up

--- server.js
+++ server.js
@@ -3,13 +3,13 @@
 const express = require('express');
 
 // Constants
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const HOST = '0.0.0.0';
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-	res.send('Hello FS2023 DevOps Course, now with adjustments, volume 2 :-)!!!');
+	res.send('Hello FS2023 DevOps Course !!!');
 });
 
 app.listen(PORT, HOST);
